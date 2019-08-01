b5f3470c5463c5c2960c644c4a21183fa00ec1a7
LAB RPL <akunabika3@gmail.com>
2019-08-01 16:51:50 +0700

    saya mengubah variabel harga

--- soal1.php
+++ soal1.php
@@ -5,7 +5,7 @@
     $total = $sakilo*$dus;
     $total2 = ($sakilo*$dus)+$jualdus;
 
-    echo "Harga jambu per kg : Rp 15.000 <br>";
+    echo "Harga jambu per kg : Rp.$sakilo <br>";
     echo "Didalam toko berisi 6 dus, dan setiap dus berisi 5kg buah jambu <br>";
     echo "Dan dus bekas bisa dijual Rp.2000 /pcs <br>";
     echo "Harga jual jambu : $total <br>";
