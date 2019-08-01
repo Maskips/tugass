3df4a057589231c8c5931ab83d0e81db8b14ecc5
LAB RPL <akunabika3@gmail.com>
2019-08-01 16:39:32 +0700

    saya nambah project pertama

---
+++ soal1.php
@@ -0,0 +1,13 @@
+<?php
+    $sakilo = 15000;
+    $dus = 6*5;
+    $jualdus = 2000*6;
+    $total = $sakilo*$dus;
+    $total2 = ($sakilo*$dus)+$jualdus;
+
+    echo "Harga jambu per kg : Rp 15.000 <br>";
+    echo "Didalam toko berisi 6 dus, dan setiap dus berisi 5kg buah jambu <br>";
+    echo "Dan dus bekas bisa dijual Rp.2000 /pcs <br>";
+    echo "Harga jual jambu : $total <br>";
+    echo "Harga jual keseluruhan : $total2";
+?>
